cb80b719093d5bd2abeab506a0087fcf3915cb5f
quantumsn <samprity.nayak9830@gmail.com>
2024-04-15 05:03:55 +0530

    JS update

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ function connectionScroll() {
 
 let aboutMeBtn = document.querySelector(".about-mee");
 
-aboutMeBtn.addEventListener("click", ()=> {
+aboutMeBtn.addEventListener("click", () => {
     aboutMeScroll();
 });
 
@@ -38,51 +38,56 @@ function aboutMeScroll() {
     let aboutMe = document.querySelector(".main");
     let aboutMePosition = aboutMe.offsetTop;
     window.scrollTo({
-        top : aboutMePosition-130,
-        behavior : "smooth"
+        top: aboutMePosition - 130,
+        behavior: "smooth"
     });
 }
 
 let projects = document.querySelectorAll(".proj");
+let body = document.querySelector("body");
 
-for(project of projects) {
-    project.addEventListener("click", function() {
+for (project of projects) {
+    project.addEventListener("click", function () {
+        let bodyWidth = body.offsetWidth;
+        if (bodyWidth < 721) {
+            alert("Please open this project on your PC for better view.");
+        }
         let id = this.getAttribute("id");
         clickBtn(id);
     });
 }
 
 function clickBtn(id) {
-    if(id == 1) {
+    if (id == 1) {
         window.location.href = "https://quantumsn.github.io/";
-    } else if(id == 2) {
+    } else if (id == 2) {
         window.location.href = "https://quantumsn.github.io/Tic-Tac-Toe-Game/";
-    }  else if(id == 3) {
+    } else if (id == 3) {
         window.location.href = "https://quantumsn.github.io/simon-says-game/";
-    } else if(id == 4) {
+    } else if (id == 4) {
         window.location.href = "https://quantumsn.github.io/rock-paper-scissors-game/";
     }
 }
 
 let socialMedias = document.querySelectorAll(".media");
 
-for(handels of socialMedias) {
-    handels.addEventListener("click", function() {
+for (handels of socialMedias) {
+    handels.addEventListener("click", function () {
         let id = this.getAttribute("id");
         connectSocial(id);
     });
 }
 
 function connectSocial(id) {
-    if(id == "insta") {
+    if (id == "insta") {
         window.location.href = "https://www.instagram.com/__public_static_void_main/";
-    } else if(id == "twitter") {
+    } else if (id == "twitter") {
         window.location.href = "https://twitter.com/NayakSampr24896";
-    } else if(id == "facebook") {
+    } else if (id == "facebook") {
         window.location.href = "not found";
-    } else if(id == "github") {
+    } else if (id == "github") {
         window.location.href = "https://github.com/quantumsn";
-    } else if(id == "linkedin") {
+    } else if (id == "linkedin") {
         window.location.href = "https://www.linkedin.com/in/samprity-nayak-85175826b/";
     }
 }
